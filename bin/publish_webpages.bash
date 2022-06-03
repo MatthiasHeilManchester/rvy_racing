4078cb48b1d18fa9f9ceabcd5012e81a49b8bc29
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2022-06-03 16:10:01 +0100

    seems to work; stuff is now published in generated_html/<series>; try out with multiple series

--- bin/publish_webpages.bash
+++ bin/publish_webpages.bash
@@ -2,7 +2,7 @@
 
 
 if [ ! -e  generated_race_data ]; then
-    echo "ERROR: Script must be run in home directory so master_race_data is"
+    echo "ERROR: Script must be run in home directory so generated_race_data is"
     echo "accessible as ./generated_race_data"
     echo " "
     echo "You are in: "`pwd`
@@ -13,8 +13,9 @@ cd generated_race_data
 
 
 # Extracting generated data for all series and races
-#https://superuser.com/questions/513304/how-to-combine-the-tar-command-with-find
-find . \( -iname 'results.html' -o -iname 'all_races_in_series.html' -o -iname 'league_table.html' \) -print0  | tar -cf generated_html_files.tar --null -T -
+# https://superuser.com/questions/513304/how-to-combine-the-tar-command-with-find
+# Also use -h in tar follow links so make a deep copy
+find . \( -iname 'results.html' -o -iname 'all_races_in_series.html' -o -iname 'league_table.html' \) -print0 | tar -hcf  generated_html_files.tar --null -T -
 
 
 
@@ -37,10 +38,17 @@ find  . -maxdepth 1 -type d -name '*[a-zA-Z]*' -exec rm -rf {} \;
 cd ..
 
 # Move and unpack updated version
-mv master_race_data/generated_html_files.tar generated_html
+mv generated_race_data/generated_html_files.tar generated_html
 cd generated_html
 tar xf generated_html_files.tar
 rm -f generated_html_files.tar
+
+# Copy across style file and general page
+dir_list=`find  . -maxdepth 1 -type d`
+for dir in `echo $dir_list`; do
+    cp ../html/style.css $dir
+    cp ../html/rouvy_racing.php $dir
+done
 
 
 
