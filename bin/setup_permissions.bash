a7e170043e5164ba3a6841ca429d11d6b2d5660a
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2022-06-06 20:24:39 +0100

    group creation instructions for setup srcipt

--- bin/setup_permissions.bash
+++ bin/setup_permissions.bash
@@ -11,6 +11,19 @@ if [ ! -e master_race_data ]; then
     exit 1
 fi
 
+echo " " 
+echo "one off preparation: "
+echo " "
+echo "sudo group add webmasters"
+echo "sudo groupadd webmasters"
+echo "sudo usermod -a -G webmasters matthias"
+echo "sudo usermod -a -G webmasters www-data"
+echo " "
+read -p "Press enter to continue"
+echo " " 
+
+
+
 sudo chgrp -R webmasters ./contributed_race_data
 sudo chgrp -R webmasters ./generated_race_data
 sudo chmod -R ug+rwx generated_race_data
