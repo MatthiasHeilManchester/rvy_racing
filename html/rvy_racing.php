b8f2f361cacde39272055ef53dbf507293a99221
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2022-06-22 13:38:56 +0100

    tweak

--- html/rvy_racing.php
+++ html/rvy_racing.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Rouvy racing</title>
+    <title>Rvy_racing</title>
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>      
   </head>
