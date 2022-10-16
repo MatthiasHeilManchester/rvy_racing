eca91014fc3d4314773940e920337e2418e57fa5
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2022-10-16 04:27:00 -0700

    adding urls of newly created races (obtained from selenium) via admin web interface now works

--- html/process_register_new_race.php
+++ html/process_register_new_race.php
@@ -4,8 +4,8 @@
 $race_number=$_POST['race_number'];
 $url_list=$_POST['urls_from_text_area'];
 
-echo "race_number: ".$race_number."<br>";
-echo "url_list2: ".$url_list."<br>";
+//echo "race_number: ".$race_number."<br>";
+//echo "url_list2: ".$url_list."<br>";
 
 
 // Extract individual urls (seperated by lines)
@@ -21,14 +21,18 @@ $fp = fopen($filename, 'w');
 
 
 
-echo "Number of urls:".$n_url."<br>";
+//echo "Number of urls:".$n_url."<br>";
 for ($i = 0; $i < $n_url; $i++)
 {
 if ( !empty($url_array[$i]) )
 {
- $the_url=str_replace(' ', '',$url_array[$i]);
+ $the_url=trim(str_replace(' ','',$url_array[$i]));
  echo "Race URL: ".$i." --".$the_url."--<br>";
- fwrite($fp, $the_url);
+ if (filter_var($the_url, FILTER_VALIDATE_URL) === FALSE)
+  {
+   die('URL not valid; bailing.');
+  }
+ fwrite($fp, $the_url."\n");
 }
 }
 
@@ -37,8 +41,44 @@ fclose($fp);
 
 
 echo "<pre>";
-$command="cat ".$filename;
+
+//$command="cat ".$filename;
+//system($command);
+
+$dir_to_be_created="../master_race_data/rvy_racing/race".sprintf('%05d', $race_number);
+$dir_prev="../master_race_data/rvy_racing/race".sprintf('%05d', ($race_number-1));
+
+echo "Dir to be created: ".$dir_to_be_created."<br>";
+echo "Previous dir     : ".$dir_prev."<br>";
+if ( file_exists( $dir_to_be_created ) )
+{
+  echo "Supposed to be created new race directory <br><br>    ".$dir_to_be_created."<br><br>already exists. Check the race number specified on the form!</br>";
+  die("Bailing");
+}
+else
+{
+  echo "Supposed to be created new race directory <br><br>    ".$dir_to_be_created."<br><br>doesn't exist yet. Race number specified on the form is ok! Yay!</br>";
+}
+
+
+if ( !file_exists( $dir_prev ) )
+{
+  echo "Supposed to be created new race directory <br><br>    ".$dir_to_be_created."<br><br>is not preceeded by an existing previous directory<br><br>    ".$dir_prev."<br><br>. Check the race number specified on the form!</br>";
+  die("Bailing");
+}
+else
+{
+  echo "Supposed to be created new race directory <br><br>    ".$dir_to_be_created."<br><br>is preceeded by an existing previous directory<br><br>    ".$dir_prev."<br><br>Race number specified on the form is ok! Yay!</br>";
+}
+echo "</pre>";
+
+// die("Bailing for now");
+
+$command="cd ../master_race_data/rvy_racing/; ../../bin/create_next_race.bash ".$filename;
 echo "Command: ".$command."<br>";
+system($command);
+//system("pwd");
+$command="cd ..; bin/bolshy_stage_races.bash rvy_racing";
 system($command);
 echo "</pre>";
 
