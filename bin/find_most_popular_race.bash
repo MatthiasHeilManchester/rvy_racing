9d479103323f7924975519cc24eca5c6f0dd83f6
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2022-11-14 16:08:31 +0000

    find most popular race works

--- bin/find_most_popular_race.bash
+++ bin/find_most_popular_race.bash
@@ -1,75 +1,72 @@
 #! /bin/bash
 
+#-------------------------------------------------------------
+# Script called from php to find number of registered riders
+# for race series: 2 args: race_series and race_number.
+# Script should only be called from php script
+#-------------------------------------------------------------
 
+# Two command line arguments
+if [ $# -ne 2 ]; then
+ echo "Two args: race_series race_number"
+ exit 1
+fi
 
-
+# Copy across into meaningful variables
 race_series=$1
 race_number=$2
 
+# Pad to get the relevant directory and extract contributed races
 padded_race_number=`printf "%05d\n" $race_number`
 race_data_dir="../generated_race_data/"$race_series"/race"$padded_race_number
-
 race_list=`cat $race_data_dir/official_race_list_items.html $race_data_dir/contributed_race_list_items.html`
 
+# Workspace
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 
-
-
+# Put into bash array
 array_assignment_command=`echo $race_list | sed 's/<li>/\"/g'| sed 's/<\/a>/<\/a>\"/g' | awk '{print "race_item_array=("$0")"}'`
-echo "STROMG " $array_assignment_command
-pwd
-echo $array_assignment_command > /tmp/.tmp1.command
-cat /tmp/.tmp1.command
-chmod a+x /tmp/.tmp1.command
-source /tmp/.tmp1.command
-#rm /tmp/.tmp1.command
-for race_item in "${race_item_array[@]}"; do
-  echo "race_item: $race_item"
-done
+echo $array_assignment_command > $tmp_dir/.tmp1.command
+chmod a+x $tmp_dir/.tmp1.command
+source $tmp_dir/.tmp1.command
+rm $tmp_dir/.tmp1.command
 
-echo "num items " ${#race_item_array[@]}
 
+# Get ready to count
 url_list=`echo $race_list | awk '{for (i=1;i<=NF;i++){if (substr($i,1,5)=="href="){end=index($i,">"); print substr($i,6,end-6)}}}'`
-
 array_assignment_string="count_array=("
 count=1
 for url in `echo $url_list`; do
-    # hierher change /tmp and clean up 
-    file="/tmp/race_file"$count".html"
-    #echo $file
-    # hierher
+    file="$tmp_dir/race_file"$count".html"
     wget --output-document=$file $url
     let count=$count+1
+    # If page doesn't have the word STARTLIST the race is over
     n_start_list=`grep -c STARTLIST $file`
     if [ $n_start_list -eq 1 ]; then
+	# If the page has the string "EVENT TIME" the race is running
 	n_event_time=`grep -c "EVENT TIME" $file`
 	if [ $n_event_time -eq 1 ]; then
-	    echo "Have event time: race seems to be running"
-	    echo "Number of registered riders: -"
-	    array_assignment_string=$array_assignment_string" \"-\""
+	    array_assignment_string=$array_assignment_string" \"[already running]\""
 	else
-	    echo "Have start list but not event time. Good!"
 	    n_registered=`grep -c registered $file`
-	    echo "Number of registered riders: "$n_registered
 	    array_assignment_string=$array_assignment_string" \"$n_registered\""
 	fi
     else
-	echo "Race seems to have finished already..."
-	echo "Number of registered riders: -"
-	array_assignment_string=$array_assignment_string" \"-\""
+	array_assignment_string=$array_assignment_string" \"[race already over]\""
     fi
+    echo " "
 done
-array_assignment_string=$array_assignment_string")"
-echo "STRING: "$array_assignment_string
-echo $array_assignment_string > /tmp/.tmp.command
-chmod a+x /tmp/.tmp.command
-source /tmp/.tmp.command
-cat /tmp/.tmp.command
-rm /tmp/.tmp.command
-count=1
-echo "<table>"
-echo "<tr> <td> Race </td> <td> Number of registered riders </td> </tr>"
 
+
+array_assignment_string=$array_assignment_string")"
+echo $array_assignment_string > $tmp_dir/.tmp.command
+chmod a+x $tmp_dir/.tmp.command
+source $tmp_dir/.tmp.command
+rm $tmp_dir/.tmp.command
+count=1
+echo "<table border=1>"
+echo "<tr style=\"background-color:yellow\"> <td> Race </td> <td> Number of currently registered riders </td> </tr>"
 for race_item in "${race_item_array[@]}"; do
-  echo "<tr> <td> "$race_item" </td> <td> "${count_array[$count]}" </td> </tr>"
+  echo "<tr> <td> "$race_item" </td> <td class=\"center_text\"> "${count_array[$count]}" </td> </tr>"
 done
 echo "</table>"
--- html/find_most_popular_race.php
+++ html/find_most_popular_race.php
@@ -5,52 +5,89 @@
 <style>
 .error {color: #FF0000;}
 </style>
+<style>
+  table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      padding: 15px;
+  }
+  .center_text {
+      text-align: center;
+  }
+</style>
 </head>
 <body>  
 
-<?php
- // GET REQUIRED RACE DATA FROM URL: THIS IS THE DATA FOR THE OFFICIAL RACE!
+  <?php
 
- // Route ID (postfix in url of route on rouvy)
- $route_id=$_GET['route_id'];
+   // MH doesn't really understand this. I'm supposed to call this but when I do
+   // flushing doesn't work at all...
+   // ob_start();
+   
+   // GET REQUIRED RACE DATA FROM URL: THIS IS THE DATA FOR THE OFFICIAL RACE!
+   
+   // Route ID (postfix in url of route on rouvy)
+   $route_id=$_GET['route_id'];
+   
+   // Route title 
+   $route_title=$_GET['route_title'];
+   
+   // Name of race series
+   $race_series=$_GET['race_series'];
+   
+   // Number of race in series
+   $race_number=$_GET['race_number'];
+   
+   // Race date (GMT)
+   $race_date_string=$_GET['race_date_string'];
+   
+   // not be needed 
+   // Remember data for the next scripts; all for the official race(s)
+   //session_start();
+   //$_SESSION['route_id'] = $route_id;
+   //$_SESSION['race_series'] = $race_series;
+   //$_SESSION['race_number'] = $race_number;
+   //$_SESSION['race_date_string'] = $race_date_string;
+   
+   ?>
+  
+  
+  <h1>Want the most popular race?</h1>
+  <h3><em>Race <?php echo $race_number ?></em> in
+    the race series <em><?php echo $race_series ?></em>: <ul><li> Route:
+	<a href="https://my.rouvy.com/virtual-routes/detail/<?php echo
+		 $route_id?>"><?php echo $route_title ?></a> 
+      <li> Date: <?php echo $race_date_string ?> (GMT).</ul>
+  </h3>
+  
+  <?php
 
- // Route title 
- $route_title=$_GET['route_title'];
+   // Loading animation
+   echo "<div id=\"loading_image\">";
+   echo "<center><img src=\"cyclist.gif\" height=300><h2>Patience please. I'm interrogating rouvy -- while cycling up a hill!</h2></center>";
+   echo "</div>";
 
- // Name of race series
- $race_series=$_GET['race_series'];
-
- // Number of race in series
- $race_number=$_GET['race_number'];
-
- // Race date (GMT)
- $race_date_string=$_GET['race_date_string'];
-
- // hierher may not be needed 
- // Remember data for the next scripts; all for the official race(s)
- session_start();
- $_SESSION['route_id'] = $route_id;
- $_SESSION['race_series'] = $race_series;
- $_SESSION['race_number'] = $race_number;
- $_SESSION['race_date_string'] = $race_date_string;
-
- ?>
+   // Make loading animation visible
+   echo "<script type=\"text/javascript\">";
+   echo "document.getElementById('loading_image').style.display = 'visible'";
+									   echo "</script>";
+   // Flush; see above
+   ob_flush();
+   ob_end_flush();
 
 
+   // Now do the actual work.
+   $command="../bin/find_most_popular_race.bash ".$race_series." ".$race_number;
+   system($command);
 
-<h2>Here's a list of the current number of signed-up racers for <em>Race <?php echo $race_number ?></em> in
-  the race series <em><?php echo $race_series ?></em> on the route
-  <a href="https://my.rouvy.com/virtual-routes/detail/<?php echo
-           $route_id?>"><?php echo $route_title ?></a> 
- held on <?php echo $race_date_string ?> (GMT).
-</h2>
+   // Hide loading animation
+   echo "<script type=\"text/javascript\">";
+   echo "document.getElementById('loading_image').style.display = 'none'";
+   echo "</script>";
 
-<?php
- //echo "I'm in directory: ".getcwd()."\n";
- $command="../bin/find_most_popular_race.bash ".$race_series." ".$race_number;
- system($command);
 ?>
 
+<h3>Please make sure you un-register on rouvy if you can't make it!</h3>
 
 
 </body>
