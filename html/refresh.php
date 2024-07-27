0d4dc646debbdc47a3a344faafa1fc7996a90620
Steven Brown <5342>
2024-07-28 02:08:28 +1000

    Error logs and a better effort to find the python_processing folder

--- html/refresh.php
+++ html/refresh.php
@@ -9,6 +9,22 @@ if (preg_match("/^([0-9]{1,3})$/", $race_number)) {
     if (!file_exists($filename)) {
         $filename = "/usr/local/bin/python3.9";
     }
+    if (!file_exists($filename)) {
+        error_log("python binary could not be found!", 0);
+    }
+
+    // get to the depth we need to be
+    for ($i = 1; $i <= 6; $i++) {
+        if (file_exists("../python_processing")) {
+            break;
+        }
+        chdir("..");
+    }
+
+    if (!file_exists("../python_processing")) {
+        error_log("python_processing folder could not be found!", 0);
+    }
+
     $command = escapeshellcmd($filename . " include_user_event.py " . $race_number . " " . $existing);
     chdir("../python_processing");
     $result =  shell_exec($command);
