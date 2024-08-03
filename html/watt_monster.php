dc7dbc372f7c43c02b4d7693406eaf7e2f760d37
Steven Brown <5342>
2024-08-03 19:49:19 +1000

    Watt monster page

---
+++ html/watt_monster.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Watt Monster</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+
+<?php
+$load_file = '';
+if (isset($_GET['month'])) {
+    $month = (int)$_GET['month'];
+    $file_glob = './generated/watt_monster_*_' . sprintf('%02d', $month) . '_*.html';
+    $result_file_list = glob($file_glob);
+    if (count($result_file_list) == 1) {
+        $load_file = $result_file_list[0];
+    }
+} ?>
+
+<div style="border: 1px solid black; background-color:rgb(250,250,250); border-radius:10px;   box-shadow: 4px 4px lightgray; padding: 10px; width:60%;">
+    <h3>(Watt-)Monster of the Month</h3>
+    Given that people may join this group at random points in the
+    season it seems unfair to have them linger at the end of the league table forever, so here's
+    a new feature: the "(Watt-)Monster of the Month" competition, a mini-league table extracted from all the races in a
+    given month. Now you can experience the end-of-season madness every month. Yay!
+</div>
+
+<div>
+    <h1>Rvy Racing: Watt Monster</h1>
+    <?php
+    if ($load_file != '') {
+        readfile($load_file);
+    }
+    else {
+        echo '<h1>No Watt-Monster found</h1>';
+    }
+    ?>
+</div>
+</body>
+</html>
