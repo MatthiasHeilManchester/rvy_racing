3d4019098edb8d8c6d4053c90309f056b58f4b42
Steven Brown <5342>
2024-08-03 19:44:18 +1000

    Add months

--- python_processing/enums.py
+++ python_processing/enums.py
@@ -21,6 +21,26 @@ class IsoDow(IntEnum):
 
 
 @enum.unique
+class RaceMonth(IntEnum):
+    JAN = 1
+    FEB = 2
+    MAR = 3
+    APR = 4
+    MAY = 5
+    JUN = 6
+    JUL = 7
+    AUG = 8
+    SEP = 9
+    OCT = 10
+    NOV = 11
+    DEC = 12
+    ALL = 13
+
+    def __format__(self, spec):
+        return f'{self._name_.capitalize()}'
+
+
+@enum.unique
 class RouvyEventType(Enum):
     RACE = 'RACE'
     GROUP_RIDE = 'GROUP_RIDE'
