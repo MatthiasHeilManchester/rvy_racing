c7adcb6a8d9bcaeb9d476170ecdc86aaf1ce8ff8
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2025-08-21 01:54:47 -0700

    latest event creator

--- python_processing/event_creator.py
+++ python_processing/event_creator.py
@@ -101,7 +101,8 @@ def create_race(race_number: int, test_mode: bool = False):
 
 if __name__ == '__main__':
     # Create a set of races based on the config...
-    create_race(15, test_mode=False)
+    create_race(16, test_mode=False)
+    create_race(17, test_mode=False)
 
     # Or
 
